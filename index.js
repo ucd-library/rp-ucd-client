1a011a4673c33595c93610d5577895a4409ba9fe
Justin Merz <jrmerz@gmail.com>
2020-07-24 11:04:58 -0700

    adding ucd-lib/rp-node-utils

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import staticController from './controllers/static.js'
 import config from './lib/config.js'
+import rpNodeUtils from '@ucd-lib/rp-node-utils';
 
+const {logger} = rpNodeUtils;
 const app = express();
 
 staticController(app);
 
 app.listen(config.server.port, () => {
-  console.log('server ready on port '+config.server.port);
+  logger.info('server ready on port '+config.server.port);
 });
