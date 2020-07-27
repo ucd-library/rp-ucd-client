558dc0c2674f6a834c354c9367b46851cda1f606
Justin Merz <jrmerz@gmail.com>
2020-07-27 14:07:19 -0700

    fix for dist js location

--- client/webpack-dist.config.cjs
+++ client/webpack-dist.config.cjs
@@ -1,7 +1,7 @@
 let config = require('@ucd-lib/cork-app-build').dist({
   root : __dirname,
   entry : 'public/elements/researcher-profiles.js',
-  dist : 'dist',
+  dist : 'dist/js',
   clientModules : 'public/node_modules'
 });
 
