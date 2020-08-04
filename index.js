9a09978c6a1bdb521a283f2ed9844d98b97a8429
Justin Merz <jrmerz@gmail.com>
2020-08-04 14:01:12 -0700

    adding jwt decode for user to static controller

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import staticController from './controllers/static.js'
 import config from './lib/config.js'
 import rpNodeUtils from '@ucd-lib/rp-node-utils';
+import cookieParser from 'cookie-parser';
 
 const {logger} = rpNodeUtils;
 const app = express();
 
+app.use(cookieParser());
 staticController(app);
 
 app.listen(config.server.port, () => {
