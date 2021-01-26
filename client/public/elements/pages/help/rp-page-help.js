9ddd26c54e76fe88af4f8f6d038f9560089536e7
Justin Merz <jrmerz@gmail.com>
2021-01-26 12:32:37 -0800

    code cleanup, updates for eslint rules

--- client/public/elements/pages/help/rp-page-help.js
+++ client/public/elements/pages/help/rp-page-help.js
@@ -1,19 +1,21 @@
 import { LitElement } from 'lit-element';
-import render from "./rp-page-help.tpl.js"
+import render from "./rp-page-help.tpl.js";
 
 import "../../components/accordian";
 
-
+/**
+ * @class RpPageHelp
+ * @description Main help page
+ */
 export default class RpPageHelp extends Mixin(LitElement)
-.with(LitCorkUtils) {
+  .with(LitCorkUtils) {
 
   static get properties() {
     return {
       visible: {type: Boolean},
       isLoggedIn: {type: Boolean},
       imgPath: {type: String}
-      
-    }
+    };
   }
 
   constructor() {
@@ -25,25 +27,43 @@ export default class RpPageHelp extends Mixin(LitElement)
     this.isLoggedIn = false;
     this.imgPath = '/images/faq/';
 
-    this.AppStateModel.get().then(e => this._onAppStateUpdate(e));
+    this.AppStateModel.get().then(e => this._renderDelay(e));
   }
 
+  /**
+   * @method _onAppStateUpdate
+   * @description bound to AppStateModel app-state-update event
+   * 
+   * @param {Object} state 
+   */
   async _onAppStateUpdate(state) {
-    requestAnimationFrame( () => this.doUpdate(state));
-   }
+    this._renderDelay(state);
+  }
 
-   async doUpdate(state) {
-    await this.updateComplete;
-    if (!this.visible) {
-      return;
-    }
-    this.isLoggedIn = APP_CONFIG.user ? true : false;
-    if (state.location.hash) {
-      let pos = this.shadowRoot.getElementById(state.location.hash);
-      if (pos) window.scrollTo(0, pos.getBoundingClientRect().top + window.pageYOffset);
-    }
-    
+  /**
+   * @method _renderDelay
+   * @description delay rendering (updating of props from state)
+   * one animation frame.  required to ensure element is visible
+   * on dame for correct scroll position
+   * 
+   * @param {Object} state 
+   */
+  _renderDelay(state) {
+    requestAnimationFrame(async () => {
+      await this.updateComplete;
 
+      if (!this.visible) {
+        return;
+      }
+      
+      this.isLoggedIn = APP_CONFIG.user ? true : false;
+      
+      if (state.location.hash) {
+        let pos = this.shadowRoot.getElementById(state.location.hash);
+        if (pos) window.scrollTo(0, pos.getBoundingClientRect().top + window.pageYOffset);
+      }
+
+    });
   }
 
 }
--- client/public/elements/pages/home/rp-page-home.js
+++ client/public/elements/pages/home/rp-page-home.js
@@ -1,5 +1,5 @@
 import { LitElement } from 'lit-element';
-import render from "./rp-page-home.tpl.js"
+import render from "./rp-page-home.tpl.js";
 
 import "@ucd-lib/cork-app-utils";
 
@@ -8,6 +8,10 @@ import "../../components/badge";
 import "../../components/person-preview";
 import "../../components/search";
 
+/**
+ * @class RpPageHome
+ * @description main home page
+ */
 export default class RpPageHome extends Mixin(LitElement)
   .with(LitCorkUtils) {
 
@@ -27,7 +31,7 @@ export default class RpPageHome extends Mixin(LitElement)
       subjectsStatus: {type: String},
       context: {type: String},
       visible: {type: Boolean}
-    }
+    };
   }
 
   constructor() {
@@ -35,7 +39,7 @@ export default class RpPageHome extends Mixin(LitElement)
     this.render = render.bind(this);
 
     this._injectModel('CollectionModel', 'AppStateModel', 'SubjectModel');
-    this.reset_properties();
+    this.resetProperties();
     this.facets = {};
     this.visible = false;
     this.academicWorksTotal = 0;
@@ -50,16 +54,25 @@ export default class RpPageHome extends Mixin(LitElement)
     this._handleResize = this._handleResize.bind(this);
   }
 
-  reset_properties(){
+  /**
+   * @method resetProperties
+   * @description reset properties on update
+   */
+  resetProperties(){
     this.people = [];
     this.academicWorks = [];
     this.subjects = [];
     this.facetsStatus = 'loading';
     this.peopleStatus = 'loading';
     this.subjectsStatus = 'loading';
-
   }
 
+  /**
+   * @method updated
+   * @description lit method called when props update
+   * 
+   * @param {Object} props 
+   */
   updated(props) {
     if (props.has('facetsStatus')) {
       if (this.facetsStatus == 'loaded') {
@@ -71,43 +84,73 @@ export default class RpPageHome extends Mixin(LitElement)
       requestAnimationFrame( () => this._handleResize());
     }
   }
+
+  /**
+   * @method connectedCallback
+   * @description lit method called when element is connected to dom
+   */
   connectedCallback() {
     super.connectedCallback();
     window.addEventListener('resize', this._handleResize);
   }
 
+  /**
+   * @method disconnectedCallback
+   * @description lit method called when element is disconnected to dom
+   */
   disconnectedCallback() {
     window.removeEventListener('resize', this._handleResize);
     super.disconnectedCallback();
   }
 
-  async doUpdate(state){
+  /**
+   * @method doUpdate
+   * @description reset props and update facets, this will rerender
+   */
+  async doUpdate(){
     await this.updateComplete;
     if (!this.visible) {
       return;
     }
-    this.reset_properties();
-    await Promise.all([this._getFacets()]);
+    this.resetProperties();
+    await this._getFacets();
   }
 
+  /**
+   * @method _onAppStateUpdate
+   * @description bound to AppStateModel app-state-update event
+   * 
+   * @param {Object} state 
+   */
   async _onAppStateUpdate(state) {
     requestAnimationFrame( () => this.doUpdate(state));
   }
 
+  /**
+   * @method _onSearch
+   * @description handle the rp-search elements new-search event.  Set the
+   * AppStateModel location
+   * 
+   * @param {Object} e 
+   */
   _onSearch(e){
     let url = "/search";
     if (e.target.nodeName == 'RP-SEARCH') {
       if (e.target.searchObject.facet.id == 'all') {
-        url = `/search?s=${encodeURIComponent(e.target.inputValue)}`
+        url = `/search?s=${encodeURIComponent(e.target.inputValue)}`;
       }
       else {
-        url = `/search/${e.target.searchObject.facet.id}?s=${encodeURIComponent(e.target.inputValue)}`
+        url = `/search/${e.target.searchObject.facet.id}?s=${encodeURIComponent(e.target.inputValue)}`;
       }
     }
-    //console.log(url);
+
     this.AppStateModel.setLocation(url);
   }
 
+  /**
+   * @method _handleResize
+   * @description bound to main window resize event
+   */
   _handleResize() {
     if (!this.visible) return;
     let w = window.innerWidth;
@@ -115,6 +158,8 @@ export default class RpPageHome extends Mixin(LitElement)
   }
 
   /**
+   * @method setPeopleWidth
+   * @description
    * Sets the text-width property of the rp-person-preview elements on this page.
    * It's the only way to get the ellipsis overflow on their titles. 
    * 
@@ -138,7 +183,7 @@ export default class RpPageHome extends Mixin(LitElement)
     // desktop min
     else if (w >= 800){
       screenPadding = 60;
-      pw = (w - screenPadding) * rightColumnWidth / peopleColumnCt - peopleColumnGutters
+      pw = (w - screenPadding) * rightColumnWidth / peopleColumnCt - peopleColumnGutters;
     }
 
     // tablet max
@@ -151,15 +196,31 @@ export default class RpPageHome extends Mixin(LitElement)
       pw = w - screenPadding - grace;
     }
 
-    pw -= avatarWidth
+    pw -= avatarWidth;
     this.peopleWidth = Math.floor(pw);
   }
 
+  /**
+   * @method _hideStatusSection
+   * @description should a given UI section be hidden based on the
+   * state of this elements property
+   * 
+   * @param {String} status state of call
+   * @param {String} field this elements stored property
+   * 
+   * @returns {Boolean}
+   */
   _hideStatusSection(status, field="status") {
     if (status == this[field]) return false;
     return true;
   }
 
+  /**
+   * @method _getPeople
+   * @description load and render a random list of people
+   * 
+   * @returns {Promise}
+   */
   async _getPeople() {
     let peopleList = await this.CollectionModel.overview('randomPeople', {limit: 4, total: this.peopleTotal});
     this.peopleStatus = peopleList.state;
@@ -170,6 +231,12 @@ export default class RpPageHome extends Mixin(LitElement)
     if (APP_CONFIG.verbose) console.log('people: ', this.people);
   }
 
+  /**
+   * @method _getSubjects
+   * @description load and render a random list of subjects
+   * 
+   * @returns {Promise}
+   */
   async _getSubjects() {
     let subjects = await this.CollectionModel.overview('randomSubjects', {limit: 10, total: this.subjectsTotal});
     this.subjectsStatus = subjects.state;
@@ -179,6 +246,12 @@ export default class RpPageHome extends Mixin(LitElement)
     
   }
 
+  /**
+   * @method _getFacets
+   * @description load and render the current overview facet list
+   * 
+   * @returns {Promise}
+   */
   async _getFacets() {
     let facetList = await this.CollectionModel.overview('facets');
     this.facetsStatus = facetList.state;
