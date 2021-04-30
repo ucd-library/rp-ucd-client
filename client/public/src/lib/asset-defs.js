4e5c5903c63445cb3a1fe0871cc2ff1b62e15178
Justin Merz <jrmerz@gmail.com>
2021-04-30 11:56:06 -0700

    fix for work index in all query

--- client/public/src/lib/asset-defs.js
+++ client/public/src/lib/asset-defs.js
@@ -244,7 +244,7 @@ class AssetDefs {
       'hasContactInfo.givenName.text^8',
       "_.organizationLabel.text^6",
       "_.personLabel.text^6",
-      "_.publicationLabel.text^6",
+      "_.workLabel.text^6",
       "_.conceptLabel.text",
       "_.subjectAreaLabel.text^2",
       "hasSubjectArea.label.text^5",
